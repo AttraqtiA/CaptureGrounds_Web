0f6f84358287c47f344b823b60a959c23ed2431e
jhwrwe <jalexander05@student.ciputra.ac.id>
2024-05-04 09:28:54 +0700

    button finnish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
                     class="bg-transparent text-yellow-400">TheMoment</mark></h1>
             <h2 class="text-white font-thin text-xl md:text-2xl lg:text-3xl">Connect With Top <br><mark
                     class="bg-transparent text-yellow-400">Videographers & Photographers</mark></h2>
+                <a href="https://www.figma.com/file/pw7eT86kzEmSIkfGUe91iz/Capture-Grounds?type=design&node-id=67%3A2&mode=design&t=PzOQ9YTSoleiprEO-1"><button class = "bg-yellow-400 text-white font-semibold p-2 rounded-lg mb-3">Click me</button></a>
         </div>
         <img src="/images/landing_2.png" class="absolute bottom-0 right-0">
     </section>
